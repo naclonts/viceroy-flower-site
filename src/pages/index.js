1e53e19ce8a1c7ceb05535999ffa5aef4749267a
Nathan Clonts <nacclonts@gmail.com>
2024-04-14 21:19:23 -0600

    updated landing page with album cover

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBandcamp, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-import LandingImage from "../images/abstraction.jpg"
+import LandingImage from "../images/arousal_jag_album_cover.jpg"
 import './index.css'
 
 
@@ -16,40 +16,30 @@ const IndexPage = () => {
 
 
       <div className="main-content">
+
+
+        {/* <figure className="image-section">
+          <img src={LandingImage} alt="The Viceroy Flower Art" style={{ maxWidth: '100%', height: 'auto' }} />
+        </figure> */}
+
+        <div className="landing-splash-section">
+          <div className="landing-text">
+            <h1>The Viceroy Flower</h1>
+          </div>
+        </div>
+
+
+
         {/* Text Section */}
-        <section className="text-section">
-          {/* Social Media Icons */}
-          <header className="header-icons">
-            <FontAwesomeIcon className="brand-icon" icon={faBandcamp} />
-            <FontAwesomeIcon className="brand-icon" icon={faInstagram} />
-            <FontAwesomeIcon className="brand-icon" icon={faTwitter} />
-          </header>
+        {/* <section className="text-section">
 
           <h1>The Viceroy Flower</h1>
           <p style={{ fontStyle: 'italic' }}>Rockin and rollin all day long</p>
-          <button
-            onClick={() => {
-              // Logic to handle the click event
-              // For example, navigating to the purchase page for the album
-              window.location.href = 'https://example.com'; // Replace with your actual album purchase link
-            }}
-          >
-            Buy Arousal Jag
-          </button>
-        </section>
+        </section> */}
 
         {/* Image Section */}
-        <figure className="image-section">
-          <img src={LandingImage} alt="The Viceroy Flower Art" style={{ maxWidth: '100%', height: 'auto' }} />
-        </figure>
       </div>
 
-      {/* Bottom Social Media Icons */}
-      <footer style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-        <FontAwesomeIcon className="brand-icon" icon={faBandcamp} />
-        <FontAwesomeIcon className="brand-icon" icon={faInstagram} />
-        <FontAwesomeIcon className="brand-icon" icon={faTwitter} />
-      </footer>
     </main>
   )
 }
